d3fa4314d53eb5b8f48f84cb4f6a3425b8ab20da
Hans Larsen <hans@hansl.ca>
2014-01-12 21:24:13 -0800

    Added timeout on requiring scripts. Args now use a common function and have a default. Use insertScript instead of document.write for the first script.

--- angularjs-script.js
+++ angularjs-script.js
@@ -4,10 +4,17 @@
  */
 (function() {
 
+// Get our script tag.
+var all_scripts = document.getElementsByTagName('script');
+var my_script = all_scripts[all_scripts.length - 1];
+
+var arg_main_module = getAttribute('app');
+var arg_path_root = getAttribute('root', '');
+var arg_timeout = getAttribute('timeout', 30000);
+
 var original_module = angular.module;
 var nb_calls = 0;
-var root = '';
-var app_module = null;
+var loaded_modules = {};
 
 function pathFromModuleName(name) {
     if (name.search(/^(https?:)?\/\/.+/) == 0) {
@@ -15,8 +22,8 @@ function pathFromModuleName(name) {
     }
     var path = name.replace('.', '/') + '.js';
 
-    if (root) {
-        path = root + '/' + path;
+    if (arg_path_root) {
+        path = arg_path_root + '/' + path;
     }
 
     return path;
@@ -32,19 +39,39 @@ function insertScript(path) {
 function maybeBootstrap() {
     if (--nb_calls == 0) {
         window.setTimeout(function() {
-            angular.bootstrap(document, [app_module]);
+            angular.bootstrap(document, [arg_main_module]);
         }, 0);
+    }
+}
+
+function getAttribute(name, defaultValue) {
+    if (my_script.attributes[name]) {
+        return my_script.attributes[name].value;
+    }
+    else if (typeof defaultValue == 'undefined') {
+        throw new Error('Need to specify an "' + name + '" attribute to angularjs-script.');
     }
 }
 
 angular.extend(angular, {
     requires: function(path, checkerFn) {
-        insertScript(pathFromModuleName(path));
+        path = pathFromModuleName(path);
+        if (loaded_modules[path]) {
+            return angular;
+        }
+
+        loaded_modules[path] = true;
+        insertScript(path);
         if (checkerFn) {
             nb_calls++;
-            window.setInterval(function() {
+            var start = +new Date();
+            var interval = window.setInterval(function() {
                 if (checkerFn()) {
                     maybeBootstrap();
+                    window.clearInterval(interval);
+                }
+                else if (new Date() - start >= arg_timeout) {
+                    throw new Error('Timed out loading "' + path + '".');
                 }
             });
         }
@@ -57,7 +84,13 @@ angular.extend(angular, {
 
         if (requires instanceof Array) {
             for (var i = 0; i < requires.length; i++) {
-                insertScript(pathFromModuleName(requires[i]));
+                var path = pathFromModuleName(requires[i]);
+                if (loaded_modules[path]) {
+                    continue;
+                }
+
+                loaded_modules[path] = true;
+                insertScript(path);
 
                 // We do it here because the call above might throw.
                 nb_calls++;
@@ -65,22 +98,14 @@ angular.extend(angular, {
         }
 
         var return_value = original_module.apply(angular, arguments);
-        maybeBootstrap();
+        maybeBootstrap();  // If we're done, bootstrap angular.
         return return_value;
     }
 });
 
 
-// Get our script tag.
-var all_scripts = document.getElementsByTagName('script');
-var my_script = all_scripts[all_scripts.length - 1];
-
 // Load the first module.
 nb_calls++;
-app_module = my_script.attributes['app'].value;
-if (my_script.attributes['root']) {
-    root = my_script.attributes['root'].value;
-}
-document.write('<script type="text/javascript" src="' + pathFromModuleName(app_module) + '"></script>');
+insertScript(pathFromModuleName(arg_main_module));
 
 })();
